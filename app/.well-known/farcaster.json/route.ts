97278f65eaa32cefdae83fbcb0efa0fd8d5e1c87
Yi Wang <wangyi98@my.yorku.ca>
2025-08-21 18:00:38 -0400

    update builder

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -39,5 +39,8 @@ export async function GET() {
       ogDescription: process.env.NEXT_PUBLIC_APP_OG_DESCRIPTION,
       ogImageUrl: process.env.NEXT_PUBLIC_APP_OG_IMAGE,
     }),
+    baseBuilder: {
+      allowedAddresses: ["0xa835412A327e14E39EbFfCFE977dE1BD261BfdA3"],
+    },
   });
 }
